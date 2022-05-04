ee310982ca2ade9b1673b239d04c7c6f0956d393
sunshine <2531227817@qq.com>
2022-05-05 01:29:49 +0800

    feat: 登录鉴权解决方案

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import installIcons from '@/icons'
-
+// 导入权限控制模块
+import './permission'
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
