baf7995e08ad63f39d30769a9ac33c39a58630c9
sunshine <2531227817@qq.com>
2022-05-06 19:17:30 +0800

    feat: 创建 MyAvatar 头像组件

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="">ProjectCard</div>
+  <div class="">
+    <my-avatar :image="$store.getters.userInfo.avatar" :height="'100px'" :width="'100px'" :hoverable="false">
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </my-avatar>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import MyAvatar from '@/components/MyAvatar'
 </script>
 
 <style lang="scss" scoped></style>
