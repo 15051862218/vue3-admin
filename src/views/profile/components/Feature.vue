a5da3348084ec453836196f99d905d62d403f2d5
sunshine <2531227817@qq.com>
2022-05-06 20:12:12 +0800

    feat: 项目介绍模块开发

--- src/views/profile/components/Feature.vue
+++ src/views/profile/components/Feature.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="">Feature</div>
+  <!-- 项目功能区域 -->
+  <div class="project-bio-section">Feature</div>
 </template>
 
-<script setup>
-import {} from 'vue'
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
