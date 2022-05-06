d975ebc87b50f1e328d0a405d08ef90059c14b38
sunshine <2531227817@qq.com>
2022-05-06 19:11:23 +0800

    feat: 个人中心模块基本布局

---
+++ src/views/profile/components/Chapter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">Chapter</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/profile/components/ProjectCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">ProjectCard</div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
