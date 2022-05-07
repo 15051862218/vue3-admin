af664d952b2af21c74094638ed3de4d4b0417db2
sunshine <2531227817@qq.com>
2022-05-07 18:41:43 +0800

    feat: 全局属性处理时间展示问题

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import installIcons from '@/icons'
+// filter
+import installFilter from '@/filter'
 // 导入权限控制模块
 import './permission'
 import './styles/index.scss'
 import i18n from '@/i18n'
 const app = createApp(App)
+installFilter(app)
 installElementPlus(app)
 installIcons(app)
 app.use(store).use(router).use(i18n).mount('#app')
